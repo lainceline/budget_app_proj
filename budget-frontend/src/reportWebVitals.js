86d7d5ec44dd69bf374c5a18270e1aae16f9ab19
Sarah Myers <sarahmyers180@gmail.com>
2024-05-28 20:39:45 -0400

    Get things working

--- budget-frontend/src/reportWebVitals.js
+++ budget-frontend/src/reportWebVitals.js
@@ -1,4 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+// src/reportWebVitals.js
+
+//import { reportWebVitals } from 'web-vitals';
+
+const report = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
@@ -10,4 +14,4 @@ const reportWebVitals = onPerfEntry => {
   }
 };
 
-export default reportWebVitals;
+export default report;
